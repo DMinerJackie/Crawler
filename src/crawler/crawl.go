20cffa2096526f805e0e886c63f5130001d822cc
Mæ <M@M.de>
2015-09-22 13:08:34 +0200

    Header check

--- src/crawler/crawl.go
+++ src/crawler/crawl.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
 
 func Crawl(link string, startHost string, wg *sync.WaitGroup, input, output chan string) {
 	defer wg.Done()
-	
+
 	counter := 0
 	//fmt.Printf("Unique: %-15d Goroutines %-5d \n", len(visited), runtime.NumGoroutine())
 
@@ -19,25 +20,35 @@ func Crawl(link string, startHost string, wg *sync.WaitGroup, input, output chan
 		Transport: transport,
 		Timeout:   timeout,
 	}
-	resp, err := client.Get(link)
+	resp, err := client.Head(link)
 	if err != nil {
-		fmt.Println("LINK ERROR: ", err)
+		fmt.Println("HEAD ERROR: ", err)
 		return
 	}
 
-	defer resp.Body.Close()
+	if strings.HasPrefix(strings.ToLower(resp.Header.Get("Content-Type")), "text") {
 
-	links := collectLinks(resp.Body)
-	//fmt.Printf("%-3d For Site: %s \n", len(links), link)
-
-	for _, foundLink := range links {
-		absoluteUrl := FixUrl(&foundLink, &link)
-		if CheckHost(&absoluteUrl, &startHost) && CheckUrl(&absoluteUrl) {
-			counter++
-			//fmt.Printf("TOTAL: %-3d FROM: %-20s INPUT: %-20s \n", len(links), link, absoluteUrl)
-			output <- absoluteUrl
+		resp, err := client.Get(link)
+		if err != nil {
+			fmt.Println("LINK ERROR: ", err)
+			return
 		}
+
+		defer resp.Body.Close()
+
+		links := collectLinks(resp.Body)
+		//fmt.Printf("%-3d For Site: %s \n", len(links), link)
+
+		for _, foundLink := range links {
+			absoluteUrl := FixUrl(&foundLink, &link)
+			if CheckHost(&absoluteUrl, &startHost) && CheckUrl(&absoluteUrl) {
+				counter++
+				//fmt.Printf("TOTAL: %-3d FROM: %-20s INPUT: %-20s \n", len(links), link, absoluteUrl)
+				wg.Add(1)
+				output <- absoluteUrl
+			}
+		}
+		//fmt.Printf("T: %-3d R: %-3d AT: %s \n", len(links), counter, link)
+		return
 	}
-	fmt.Printf("T: %-3d R: %-3d AT: %s \n", len(links), counter, link)
-	return
 }
