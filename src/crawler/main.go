7acb402334864391d812dbdbd44bb30c922ec391
Mæ <M@M.de>
2015-09-22 12:56:50 +0200

    Waitgroup changed

--- src/crawler/main.go
+++ src/crawler/main.go
@@ -9,54 +9,60 @@ import (
 	"time"
 )
 
-var workers = 10
-var input = make(chan string, 100000)
-var output = make(chan string, 100000)
+var workers = 1
+var input = make(chan string, 10000)
+var output = make(chan string, 1)
 var visited = make(map[string]bool)
+var counter = 1
+var throttle = time.Tick(5000 * time.Millisecond)
 
 /*
 	Start
 */
 
 func main() {
-	//visited[`https://www.roller.de/mhs-order-status`] = true
 
 	start := time.Now()
 
 	// Waitgroup to know when all Goroutines are closed
 	var wg sync.WaitGroup
 
-	startPage := "http://www.roller.de/"
+	startPage := "http://www.ebay.de/"
 	startUrl, _ := url.Parse(startPage)
 	startHost := startUrl.Host
 	fmt.Printf("Crawling %s @ Host %s \n", startUrl, startHost)
 
+	wg.Add(1)
+	output <- startPage
+
 	// Create the number of workers
 	for i := 0; i < workers; i++ {
-		//fmt.Printf("Worker created: %d \n", i)
-		//wg.Add(1)
 		go worker(i, startHost, &wg, input, output)
 	}
 
-	wg.Add(1)
-	go Crawl(startPage, startHost, &wg, input, output)
-
 	go func() {
-		for link := range output {
-			if visited[link] == false {
-				visited[link] = true
-				//fmt.Println("OUTPUT: " + link)
-				input <- link
+		for {
+			select {
+			case link := <-output:
+				if visited[link] == false {
+					visited[link] = true
+					fmt.Printf("%-3d # %s \n", counter, link)
+					counter++
+					input <- link
+					wg.Done()
+				} else {
+					wg.Done()
+				}
 			}
 		}
 	}()
 
 	go func() {
 		wg.Wait()
-		//close(input)
+		close(input)
 		elapsed := time.Since(start)
 		fmt.Printf("\n%d links in %f seconds\n", len(visited), elapsed.Seconds())
-		//os.Exit(0)
+		os.Exit(0)
 		//ExportToCSV(startHost, visited)
 		//fmt.Printf("\nCSV file created for %s\n", startHost)
 	}()
@@ -67,8 +73,12 @@ func main() {
 
 func worker(i int, startHost string, wg *sync.WaitGroup, input, output chan string) {
 	//fmt.Printf("Worker started: %d \n", i)
-	for link := range input {
-		wg.Add(1)
-		Crawl(link, startHost, wg, input, output)
+	for {
+		select {
+		case link := <-input:
+			//<-throttle
+			wg.Add(1)
+			Crawl(link, startHost, wg, input, output)
+		}
 	}
 }
