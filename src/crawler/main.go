75cdf63b347e11e74088a7f67f847e36b685e575
Mæ <M@M.de>
2015-09-30 16:49:25 +0200

    logs enabled

--- src/crawler/main.go
+++ src/crawler/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net/url"
 	"os"
@@ -9,30 +10,47 @@ import (
 	"time"
 )
 
-var workers = 1
-var input = make(chan string, 10000)
-var output = make(chan string, 1)
+var input = make(chan string, 1000000)
+var output = make(chan string, 1000000)
 var visited = make(map[string]bool)
-var counter = 1
-var throttle = time.Tick(5000 * time.Millisecond)
+var counter = 0
+var errcounter = 0
+var throttle = time.Tick(100 * time.Millisecond)
 
 /*
 	Start
 */
 
 func main() {
-
 	start := time.Now()
+
+	linkPtr := flag.String("url", "example.de", "site")
+	numbWorkerPtr := flag.Int("w", 1, "connections")
+	logPtr := flag.Int("log", 1, "0-4")
+
+	flag.Parse()
+
+	workers := *numbWorkerPtr
+	link := *linkPtr
+	logLevel := int32(*logPtr)
+
+	// LOGGING
+	file, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		Error.Println("failed open file")
+	}
+	defer file.Close()
 
 	// Waitgroup to know when all Goroutines are closed
 	var wg sync.WaitGroup
 
-	startPage := "http://www.ebay.de/"
+	startPage := "http://www." + link
 	startUrl, _ := url.Parse(startPage)
 	startHost := startUrl.Host
-	fmt.Printf("Crawling %s @ Host %s \n", startUrl, startHost)
 
-	wg.Add(1)
+	setLogLevel(logLevel, file)
+	Info.Printf("Start: %s : %d workers : loglevel %d", startHost, workers, logLevel)
+
 	output <- startPage
 
 	// Create the number of workers
@@ -46,22 +64,36 @@ func main() {
 			case link := <-output:
 				if visited[link] == false {
 					visited[link] = true
-					fmt.Printf("%-3d # %s \n", counter, link)
 					counter++
+					//fmt.Printf("%-3d # %s \n", counter, link)
+					if counter%1000 == 0 {
+						//Info.Printf("Crawled: %-5d", counter)
+					}
 					input <- link
-					wg.Done()
-				} else {
-					wg.Done()
 				}
 			}
 		}
 	}()
 
+	//	go func() {
+	//		for {
+	//			if len(input) == 0 && len(output) == 0 {
+	//				close(input)
+	//				close(output)
+	//				elapsed := time.Since(start)
+	//
+	//				os.Exit(0)
+	//			}
+	//		}
+	//	}()
+
 	go func() {
 		wg.Wait()
+		fmt.Println("CLOSED")
 		close(input)
+		close(output)
 		elapsed := time.Since(start)
-		fmt.Printf("\n%d links in %f seconds\n", len(visited), elapsed.Seconds())
+		Info.Printf("Stop: %d visited: %d failed: %f seconds", counter, errcounter, elapsed.Seconds())
 		os.Exit(0)
 		//ExportToCSV(startHost, visited)
 		//fmt.Printf("\nCSV file created for %s\n", startHost)
@@ -72,12 +104,11 @@ func main() {
 }
 
 func worker(i int, startHost string, wg *sync.WaitGroup, input, output chan string) {
-	//fmt.Printf("Worker started: %d \n", i)
 	for {
 		select {
 		case link := <-input:
-			//<-throttle
-			wg.Add(1)
+		wg.Add(1)
+			<-throttle	
 			Crawl(link, startHost, wg, input, output)
 		}
 	}
