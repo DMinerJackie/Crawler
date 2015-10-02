a194bb853dc178df3360876af8f089bd4d6fa7dc
Mæ <M>
2015-10-02 05:39:46 +0200

    addedd "Ever" logging type that always logs

--- src/crawler/log.go
+++ src/crawler/log.go
@@ -9,42 +9,58 @@ import (
 )
 
 var (
-	Debug   *log.Logger
+	Ever    *log.Logger
 	Info    *log.Logger
 	Warning *log.Logger
 	Error   *log.Logger
+	Debug   *log.Logger
 )
 
 // turnOnLogging configures the logging writers.
 func setLogLevel(logLevel int32, fileHandle io.Writer) {
-	debugHandle := ioutil.Discard
+	everHandle := ioutil.Discard
 	infoHandle := ioutil.Discard
 	warnHandle := ioutil.Discard
 	errorHandle := ioutil.Discard
+	debugHandle := ioutil.Discard
+	everHandle = os.Stdout
 
 	if logLevel == 1 {
-		debugHandle = os.Stdout
 		infoHandle = os.Stdout
-		warnHandle = os.Stdout
-		errorHandle = os.Stderr
 	}
 
 	if logLevel == 2 {
 		infoHandle = os.Stdout
 		warnHandle = os.Stdout
-		errorHandle = os.Stderr
 	}
 
 	if logLevel == 3 {
+		infoHandle = os.Stdout
 		warnHandle = os.Stdout
 		errorHandle = os.Stderr
 	}
 
 	if logLevel == 4 {
+		warnHandle = os.Stdout
 		errorHandle = os.Stderr
 	}
 
-	if fileHandle != nil && logLevel != 0 {
+	if logLevel == 5 {
+		errorHandle = os.Stderr
+	}
+
+	if logLevel == 6 {
+		infoHandle = os.Stdout
+		warnHandle = os.Stdout
+		errorHandle = os.Stderr
+		debugHandle = os.Stdout
+	}
+
+	if fileHandle != nil && logLevel != -1 {
+		if everHandle == os.Stdout {
+			everHandle = io.MultiWriter(fileHandle, everHandle)
+		}
+
 		if debugHandle == os.Stdout {
 			debugHandle = io.MultiWriter(fileHandle, debugHandle)
 		}
@@ -62,8 +78,9 @@ func setLogLevel(logLevel int32, fileHandle io.Writer) {
 		}
 	}
 
-	Debug = log.New(debugHandle, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Ever = log.New(everHandle, "LOG: ", log.Ldate|log.Ltime)
+	Debug = log.New(debugHandle, "DEBUG: ", log.Ldate|log.Ltime)
+	Info = log.New(infoHandle, "INFO: ", log.Ldate|log.Ltime)
 	Warning = log.New(warnHandle, "WARNING: ", log.Ldate|log.Ltime)
 	Error = log.New(errorHandle, "ERROR: ", log.Ldate|log.Ltime)
 
